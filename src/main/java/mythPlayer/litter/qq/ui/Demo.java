627d9bb402a9f1e488a188907314bc121ebf739a
Jarvis-JunZhu <350295606@qq.com>
2017-06-08 15:32:18 +0800

    蛋散

---
+++ src/main/java/mythPlayer/litter/qq/ui/Demo.java
@@ -0,0 +1,42 @@
+/*
+ * Created by JFormDesigner on Thu Jun 01 15:16:38 CST 2017
+ */
+
+package mythPlayer.litter.qq.ui;
+
+import java.awt.*;
+import javax.swing.*;
+import net.miginfocom.swing.*;
+
+/**
+ * @author unknown
+ */
+public class Demo extends JFrame {
+    public Demo() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new MigLayout(
+            "hidemode 3",
+            // columns
+            "[fill]" +
+            "[fill]",
+            // rows
+            "[]" +
+            "[]" +
+            "[]"));
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - unknown
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
---
+++ src/main/java/mythPlayer/litter/qq/ui/Demo01.java
@@ -0,0 +1,20 @@
+package mythPlayer.litter.qq.ui;
+
+import javax.swing.*;
+
+/**
+ * Created by Jarvis on 2017/6/1.
+ */
+public class Demo01 {
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
+    private JButton button10;
+}
